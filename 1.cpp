e13a29ab6da7171542b903723bd0c5a7585987ff
NannStillLearning <153050302+NannStillLearning@users.noreply.github.com>
2023-12-06 10:48:17 +0700

    first commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main {
+    int a1, a2, hasil;
+
+}
+{
+    cout<<"masukan angka pertama : "cin>>a1;
+    cout<<"masukan angka dua : "cin>>a2;
+    hasil = a1+a2;
+    cout<<"hasilnya adalah : "hasil;
+return 0;
+}
---
+++ belajarjs1.js
@@ -0,0 +1,10 @@
+function ubahData(){
+    document.getElementById("P1").innerHTML = "sudah belajar";
+    alert("sudah belajar")
+}
+function getDataP1(){
+    var data = document.getElementById("p1").innerHTML;
+    alert(data);
+}
+
+
